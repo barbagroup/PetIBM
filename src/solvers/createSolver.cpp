20df28a74ccdf41b0b2d5c0b02b9245bae66dcf5
Anush Krishnan <k.anush@gmail.com>
2014-06-24 12:29:26 -0400

    Print simulation info while running code.
     * Remove print statements from createSolver.
     * writeSimulationInfo is now one function templated over dim.

--- src/solvers/createSolver.cpp
+++ src/solvers/createSolver.cpp
@@ -9,16 +9,12 @@ std::unique_ptr< NavierStokesSolver<dim> > createSolver(std::string folder, Flow
 		PetscPrintf(PETSC_COMM_WORLD, "ERROR: Check the number of dimensions in the input files.\n");
 		exit(0);
 	}
-	PetscPrintf(PETSC_COMM_WORLD, "gamma: %f, zeta: %f, alphaExplicit: %f, alphaImplicit: %f\n", SP->gamma, SP->zeta, SP->alphaExplicit, SP->alphaImplicit);
-	PetscPrintf(PETSC_COMM_WORLD, "Solver type selected: ");
 	switch(SP->solverType)
 	{
 		case NAVIER_STOKES:
-			PetscPrintf(PETSC_COMM_WORLD, "Navier-Stokes\n");
 			return std::unique_ptr< NavierStokesSolver<dim> >(new NavierStokesSolver<dim>(folder, FD, SP, CM));
 			break;
 		case TAIRA_COLONIUS:
-			PetscPrintf(PETSC_COMM_WORLD, "Taira and Colonius\n");
 			return std::unique_ptr< TairaColoniusSolver<dim> >(new TairaColoniusSolver<dim>(folder, FD, SP, CM));
 			break;
 		default:

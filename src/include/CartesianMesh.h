a8c394be660887677df68b49e4679107f58af73e
Anush Krishnan <k.anush@gmail.com>
2014-06-25 23:01:03 -0400

    Document CartesianMesh.h.

--- src/include/CartesianMesh.h
+++ src/include/CartesianMesh.h
@@ -1,3 +1,8 @@
+/***************************************************************************//**
+* \file
+* \brief Header to define class CartesianMesh
+*/
+
 #if !defined(CARTESIAN_MESH_H)
 #define CARTESIAN_MESH_H
 
@@ -5,13 +10,27 @@
 #include <string>
 #include <vector>
 
+/***************************************************************************//**
+* \brief Store the details of the cartesian mesh used to discretize the domain
+*/
 class CartesianMesh
 {
 public:
-	PetscInt               nx, ny, nz;
-	std::vector<PetscReal> x, y, z;
-	std::vector<PetscReal> dx, dy, dz;
+	PetscInt               nx, ///< number of cells in the x-direction
+	                       ny, ///< number of cells in the y-direction
+	                       nz; ///< number of cells in the z-direction
+	
+	std::vector<PetscReal> x,  ///< x-coordinates of the nodes
+	                       y,  ///< y-coordinates of the nodes
+	                       z;  ///< z-coordinates of the nodes
+	
+	std::vector<PetscReal> dx, ///< cell-widths along the x-direction
+	                       dy, ///< cell-widths along the y-direction 
+	                       dz; ///< cell-widths along the z-direction
     
+    /***********************************************************************//**
+	* \brief Reads an input file and initializes the cartesian mesh
+	*/
 	CartesianMesh(std::string fileName);
 };
 

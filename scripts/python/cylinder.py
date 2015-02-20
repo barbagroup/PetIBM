a35e1c8f23c274bf1aa7b1c193256321e88a2617
mesnardo <mesnardo@gwu.edu>
2015-02-19 19:00:58 -0500

    Rewrite script to generate a cylinder

--- scripts/python/cylinder.py
+++ scripts/python/cylinder.py
@@ -1,59 +1,71 @@
 #!/usr/bin/env python
 
+# file: cylinder.py
+# author: Anush Krishnan (anush@bu.edu), Olivier Mesnard (mesnardo@gwu.edu)
+# description: Generates coordinates of a 3D cylinder.
+
+
 import argparse
-import numpy as np
+import os
+
+import numpy
+
 
 def read_inputs():
-	"""Creates the parser and parses the command-line."""
-	# create the parser
-	parser = argparse.ArgumentParser(description='Generates a cylindrical body ',
-									 formatter_class= \
-									 argparse.ArgumentDefaultsHelpFormatter)
-	# add arguments to the parser
-	parser.add_argument('-R', dest='R', type=float, default=0.5,
-						help='radius of the cylinder')
-	parser.add_argument('-zmin', dest='zmin', type=float, default=-3.,
-						help='minimum z-coordinate')
-	parser.add_argument('-zmax', dest='zmax', type=float, default=3.,
-						help='maximum z-coordinate')
-	parser.add_argument('-x0', dest='x0', type=float, default=0.0,
-						help='x-coordinate of the center of the cylinder')
-	parser.add_argument('-y0', dest='y0', type=float, default=0.0,
-						help='y-coordinate of the center of the cylinder')
-	parser.add_argument('-ds', dest='ds', type=str, default='0.015',
-						help='mesh spacing')
-	parser.add_argument('-f', '--filename', dest='filename', type=str,
-						default='',
-						help='name of the file generated')
-	return parser.parse_args()
+  """Creates the parser and parses the command-line."""
+  # create parser
+  parser = argparse.ArgumentParser(description='Generates a cylindrical body',
+                        formatter_class= argparse.ArgumentDefaultsHelpFormatter)
+  # add arguments to parser
+  parser.add_argument('--radius', '-r', dest='radius', type=float, default=0.5,
+                      help='radius of the cylinder')
+  parser.add_argument('--limits', '-l', dest='limits', type=float, nargs='+',
+                      default=[-3.0, 3.0],
+                      help='longitudinal limits of the cylinder')
+  parser.add_argument('--center', '-c', dest='center', type=float, nargs='+',
+                      default=[0.0, 0.0],
+                      help='coordinates of the center of the cylinder')
+  parser.add_argument('-ds', dest='ds', type=float, default='0.015',
+                      help='mesh-spacing')
+  parser.add_argument('--name', dest='file_name', type=str, default=None,
+                      help='name of the output file (without extension)')
+  parser.add_argument('--save-dir', dest='save_dir', type=str, 
+                      default=os.getcwd(),
+                      help='directory where file will be saved')
+  return parser.parse_args()
 
 def main():
-	args = read_inputs()
-	R = args.R
-	zmax = args.zmax
-	zmin = args.zmin
-	ds = float(args.ds)
-	if args.filename:
-		filename = args.filename
-	else:
-		filename = 'cylinder_' + args.ds + '.body'
+  """Generates the coordinates of a 3D cylinder and writes into file."""
+  # parse command-line
+  args = read_inputs()
+  
+  R = args.radius
+  zmin = args.limits[0]
+  zmax = args.limits[1]
+  ds = args.ds
+  
+  length = zmax-zmin
+  nz = int(round(length/ds))
+  if abs(nz-length/ds) > 1.0E-08:
+    print ('Choose a mesh spacing such that the cylinder length is an '
+           'integral multiple of it')
+    print ('%s-direction: length l=%g \t spacing ds=%g \t l/ds=%g' 
+           % (length, ds, length/ds))
+    sys.exit()
 
-	nz = int(round((zmax-zmin)/ds))
-	if np.abs((zmax-zmin)/ds - nz) > 1e-8:
-		print "Choose a mesh spacing such that the cylinder height is an integral multiple of it!"
-		print "z-direction: %s/%s = %s" % (str(zmax-zmin), str(ds), str((zmax-zmin)/ds))
-	
-	nb = int(np.ceil(2*np.pi*R/ds))
-	total = nb*nz
-	f = open(filename, 'w')
-	f.write("%d\n" % total)
-	for k in range(nz):
-		z = zmin+(k+0.5)*ds
-		for i in range(nb):
-			x = R*np.cos(i*2*np.pi/nb)
-			y = R*np.sin(i*2*np.pi/nb)
-			f.write("%f\t%f\t%f\n" % (x,y,z))
-	f.close()
+  nc = int(numpy.ceil(2.0*numpy.pi*R/ds))
+
+  if not args.file_name:
+    args.file_name = 'cylinder_%g.body' % args.ds
+
+  with open(args.save_dir+'/'+args.file_name, 'w') as outfile:
+    outfile.write('%d\n' % (nc*nz))
+    i = numpy.arange(nc)
+    x, y = R*numpy.cos(2.0*numpy.pi*i/nc), R*numpy.sin(2.0*numpy.pi*i/nc)
+    for k in xrange(nz):
+      z = zmin + (k+0.5)*ds * numpy.ones(nc)
+      numpy.savetxt(outfile, numpy.c_[x, y, z], fmt='%.6f', delimiter='\t')
+
 
 if __name__ == "__main__":
-	main()
+  main()

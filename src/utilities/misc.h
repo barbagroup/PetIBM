b45de7f6dc6562c386b8aaac5c5e057d575a060a
Pi-Yueh Chuang <pychuang@gwu.edu>
2017-03-30 18:49:08 -0400

    add looping helper functions; make misc function inline
    We remove the misc.cpp, and move all function definitions in misc
    namespace to misc.h, so that we can make these simple helper functions
    inline.

--- src/utilities/misc.h
+++ src/utilities/misc.h
@@ -10,6 +10,7 @@
 // here goes C++ STL
 # include <vector>
 # include <algorithm>
+# include <functional>
 # include <cmath>
 
 // here goes PETSc
@@ -32,8 +33,22 @@ namespace misc
      *
      * \todo discuss if the point is in the first or the last cell, will PetIBM work?
      */
-    PetscErrorCode findCell1D(
-            const PetscReal &p, const std::vector<PetscReal> &x, PetscInt &loc);
+    inline PetscErrorCode findCell1D(
+            const PetscReal &p, const std::vector<PetscReal> &x, PetscInt &loc)
+    {
+        PetscFunctionBeginUser;
+
+        if (x.back() < p || x[0] > p)
+            SETERRQ1(PETSC_COMM_WORLD, 56, 
+                    "body coordinate %e is outside domain !", p);
+
+        auto it = std::lower_bound(x.begin(), x.end(), p);
+
+        loc = it - x.begin() - 1;
+
+        PetscFunctionReturn(0);
+    }
+
 
     /** \brief  calculate and returned cell sizes of stretched grid in one direction.
      *
@@ -45,7 +60,86 @@ namespace misc
      *
      * \return PetscErrorCode
      */
-    PetscErrorCode stretchGrid(
+    inline PetscErrorCode stretchGrid(
             const PetscReal &bg, const PetscReal &ed, 
-            const PetscInt &n, const PetscReal &r, types::RealVec1D &dL);
+            const PetscInt &n, const PetscReal &r, types::RealVec1D &dL)
+    {
+        PetscFunctionBeginUser;
+
+        dL.resize(n);
+
+        PetscReal       h;
+
+        // calculate the size of the first cell
+        dL[0] = (ed - bg) * (r - 1.0) / (std::pow(r, n) - 1.0);
+
+        // dL[i] = dL[i-1] * r
+        for(auto it=dL.begin()+1; it<dL.end(); ++it) *it = *(it -1) * r;
+
+        PetscFunctionReturn(0);
+    }
+
+
+    /** \brief a helper struct to make looping function easier. */
+    struct LoopBound {const PetscInt &bg, &ed;};
+
+
+    /**
+     * \brief a helper function to carry out a double loop on a given function.
+     *
+     * \param bound1 bound for the outer loop.
+     * \param bound2 bound for the inner loop.
+     * \param f the kernel that will be called in the function.
+     *
+     * \return PetscErrorCode.
+     */
+    inline PetscErrorCode doubleLoops(
+            const LoopBound &bound1, const LoopBound &bound2, 
+            const std::function<PetscErrorCode(const PetscInt &, const PetscInt &)> &f)
+    {
+        PetscFunctionBeginUser;
+
+        PetscErrorCode      ierr;
+
+        for(PetscInt i=bound1.bg; i<bound1.ed; i++)
+            for(PetscInt j=bound2.bg; j<bound2.ed; j++)
+            {
+                ierr = f(i, j); CHKERRQ(ierr);
+            }
+                
+
+        PetscFunctionReturn(0);
+    }
+
+
+    /**
+     * \brief a helper function to carry out a triple loop on a given function.
+     *
+     * \param bound1 bound for the most outer loop.
+     * \param bound2 bound for the middle loop.
+     * \param bound3 bound for the most inner loop.
+     * \param f the kernel that will be called in the function.
+     *
+     * \return PetscErrorCode.
+     */
+    inline PetscErrorCode tripleLoops(
+            const LoopBound &bound1, const LoopBound &bound2,
+            const LoopBound &bound3,
+            const std::function<PetscErrorCode(
+                const PetscInt &, const PetscInt &, const PetscInt &)> &f)
+    {
+        using namespace std::placeholders;
+
+        PetscFunctionBeginUser;
+
+        PetscErrorCode      ierr;
+
+        for(PetscInt i=bound1.bg; i<bound1.ed; ++i)
+        {
+            ierr = doubleLoops(bound2, bound3, std::bind(f, i, _1, _2));
+            CHKERRQ(ierr);
+        }
+
+        PetscFunctionReturn(0);
+    }
 }

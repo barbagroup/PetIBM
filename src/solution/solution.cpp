c20d8de694a0674ac11b22907b8acd1e916f259e
Pi-Yueh Chuang <pychuang@gwu.edu>
2018-01-16 18:23:45 -0500

    Doxygen documentation for the module of solution holders

--- src/solution/solution.cpp
+++ src/solution/solution.cpp
@@ -1,8 +1,10 @@
-/*
- * solution.cpp
- * Copyright (C) 2017 Pi-Yueh Chuang <pychuang@gwu.edu>
- *
- * Distributed under terms of the MIT license.
+/**
+ * \file solution.cpp
+ * \brief Implementations of createSolution and members of SolutionBase.
+ * \author Anush Krishnan (anus@bu.edu)
+ * \author Olivier Mesnard (mesnardo@gwu.edu)
+ * \author Pi-Yueh Chuang (pychuang@gwu.edu)
+ * \copyright MIT.
  */
 
 # include <petibm/solution.h>

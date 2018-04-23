c414d6c6c0a94f8184d468371b4764fda5ffc7be
Pi-Yueh Chuang <pychuang@gwu.edu>
2018-04-23 18:34:08 -0400

    make the XDMF works for both ParaView and VisIt
    With 2DRectMesh and VXVY, ParaView and VisIt have different reading behaviors
    regarding the order of axes. But with 3DRectMesh and VXVYVZ, there's no
    such problem. So we now use 3DRectMesh and VXVYVZ for everything, even
    it's a 2D simulation. For 2D simulations, a dummy z-axis grid line is
    added with only one grid point, which has 0.0 coordinate.

--- applications/createxdmf/main.cpp
+++ applications/createxdmf/main.cpp
@@ -21,48 +21,48 @@
 /**
  * \defgroup createxdmf Post-processing utility: createxdmf
  * \brief A post-processing utility that creates XDMF files for visualization.
- * 
- * This is a helper utility built with PetIBM components, and it creates XDMF 
+ *
+ * This is a helper utility built with PetIBM components, and it creates XDMF
  * files based on the HDF5 files produced by
  * \ref nssolver "Navier-Stokes solver",
- * \ref tairacolonius "IBPM solver", and 
+ * \ref tairacolonius "IBPM solver", and
  * \ref decoupledibpm "decoupled IBPM solver".
- * XDMF files can be used for visualization in 
+ * XDMF files can be used for visualization in
  * [VisIt](https://wci.llnl.gov/simulation/computer-codes/visit/)
- * 
+ *
  * If readers are interested in using this utility,
- * please refer to 
+ * please refer to
  * \ref md_doc_markdowns_runpetibm "Running PetIBM",
  * \ref md_doc_markdowns_examples2d "2D Exmaples", and
  * \ref md_doc_markdowns_examples3d "3D Examples".
- * 
+ *
  * \ingroup apps
  */
 
 PetscErrorCode writeSingleXDMF(
-        const std::string &directory, const std::string &name, 
+        const std::string &directory, const std::string &name,
         const PetscInt &dim, const petibm::type::IntVec1D &n,
         const PetscInt &bg, const PetscInt &ed, const PetscInt &step);
 
 int main(int argc, char **argv)
 {
-    
+
     PetscErrorCode      ierr;
-    
+
     ierr = PetscInitialize(&argc, &argv, nullptr, nullptr); CHKERRQ(ierr);
-    
-    
-    
+
+
+
     YAML::Node  setting;
-    
+
     ierr = petibm::parser::getSettings(setting); CHKERRQ(ierr);
-    
+
     petibm::type::Mesh  mesh;
-    
+
     ierr = petibm::mesh::createMesh(PETSC_COMM_WORLD, setting, mesh); CHKERRQ(ierr);
-    
-    
-    
+
+
+
     PetscInt                    bg;
     PetscInt                    ed;
     PetscInt                    step;
@@ -73,43 +73,43 @@ int main(int argc, char **argv)
     if (! isSet) bg = setting["parameters"]["startStep"].as<PetscInt>(0);
 
     ierr = PetscOptionsGetInt(nullptr, nullptr, "-ed", &ed, &isSet); CHKERRQ(ierr);
-    if (! isSet) ed = bg + setting["parameters"]["nt"].as<PetscInt>(); 
+    if (! isSet) ed = bg + setting["parameters"]["nt"].as<PetscInt>();
 
     ierr = PetscOptionsGetInt(nullptr, nullptr, "-step", &step, &isSet); CHKERRQ(ierr);
-    if (! isSet) step = setting["parameters"]["nsave"].as<PetscInt>(); 
-    
-    
-    
+    if (! isSet) step = setting["parameters"]["nsave"].as<PetscInt>();
+
+
+
     // u
     ierr = writeSingleXDMF(setting["directory"].as<std::string>(),
             "u", mesh->dim, mesh->n[0], bg, ed, step); CHKERRQ(ierr);
-    
+
     // v
     ierr = writeSingleXDMF(setting["directory"].as<std::string>(),
             "v", mesh->dim, mesh->n[1], bg, ed, step); CHKERRQ(ierr);
-    
+
     // p
     ierr = writeSingleXDMF(setting["directory"].as<std::string>(),
             "p", mesh->dim, mesh->n[3], bg, ed, step); CHKERRQ(ierr);
-    
+
     // wz
     petibm::type::IntVec1D  wn(3);
-    
+
     wn[0] = mesh->n[4][0]; wn[1] = mesh->n[4][1]; wn[2] = mesh->n[3][2];
     ierr = writeSingleXDMF(setting["directory"].as<std::string>(),
             "wz", mesh->dim, wn, bg, ed, step); CHKERRQ(ierr);
-    
+
     if (mesh->dim == 3)
     {
         // w
-        ierr = writeSingleXDMF(setting["directory"].as<std::string>(), 
+        ierr = writeSingleXDMF(setting["directory"].as<std::string>(),
                 "w", mesh->dim, mesh->n[2], bg, ed, step); CHKERRQ(ierr);
-        
+
         // wx
         wn[0] = mesh->n[3][0]; wn[1] = mesh->n[4][1]; wn[2] = mesh->n[4][2];
         ierr = writeSingleXDMF(setting["directory"].as<std::string>(),
                 "wx", mesh->dim, wn, bg, ed, step); CHKERRQ(ierr);
-        
+
         // wy
         wn[0] = mesh->n[4][0]; wn[1] = mesh->n[3][1]; wn[2] = mesh->n[4][2];
         ierr = writeSingleXDMF(setting["directory"].as<std::string>(),
@@ -120,94 +120,108 @@ int main(int argc, char **argv)
     ierr = mesh->destroy(); CHKERRQ(ierr);
 
     ierr = PetscFinalize(); CHKERRQ(ierr);
-    
+
     return 0;
 } // main
 
 
 PetscErrorCode writeSingleXDMF(
-        const std::string &directory, const std::string &name, 
+        const std::string &directory, const std::string &name,
         const PetscInt &dim, const petibm::type::IntVec1D &n,
         const PetscInt &bg, const PetscInt &ed, const PetscInt &step)
 {
     using namespace petibm;
-    
+
     PetscFunctionBeginUser;
-    
+
     PetscErrorCode  ierr;
 
     PetscViewer         viewer;
-    
+
     std::string         file = directory + "/" + name + ".xmf";
-    
+
     ierr = PetscViewerASCIIOpen(
             PETSC_COMM_WORLD, file.c_str(), &viewer); CHKERRQ(ierr);
-    
+
     // write header
-    ierr = PetscViewerASCIIPrintf(viewer, 
-            "<?xml version=\'1.0\' encoding=\'ASCII\'?>\n\n"); CHKERRQ(ierr);
-    
-    
+    ierr = PetscViewerASCIIPrintf(viewer,
+            "<?xml version=\'1.0\' ?>\n\n"); CHKERRQ(ierr);
+
+
     // write macro definitions
-    ierr = PetscViewerASCIIPrintf(viewer, 
+    ierr = PetscViewerASCIIPrintf(viewer,
             "<!DOCTYPE Xdmf SYSTEM \"Xdmf.dtd\" [\n"); CHKERRQ(ierr);
-    ierr = PetscViewerASCIIPrintf(viewer, 
+    ierr = PetscViewerASCIIPrintf(viewer,
             "    <!ENTITY CaseDir \"%s\">\n", "./"); CHKERRQ(ierr);
-    for(int i=0; i<dim; ++i)
+
+    // always use 3D XDMF format, so both Visit and Paraview works
+    for(int i=0; i<3; ++i)
     {
-        ierr = PetscViewerASCIIPrintf(viewer, "    <!ENTITY N%s \"%d\">\n", 
+        ierr = PetscViewerASCIIPrintf(viewer, "    <!ENTITY N%s \"%d\">\n",
                 type::dir2str[type::Dir(i)].c_str(), n[i]); CHKERRQ(ierr);
     }
     ierr = PetscViewerASCIIPrintf(viewer, "]>\n\n"); CHKERRQ(ierr);
-    
+
     // write Xdmf block
-    ierr = PetscViewerASCIIPrintf(viewer, 
+    ierr = PetscViewerASCIIPrintf(viewer,
             "<Xdmf Version=\"2.2\">\n"); CHKERRQ(ierr);
     ierr = PetscViewerASCIIPrintf(viewer, "    "
             "<Information Name=\"MeteData\" Value=\"ID-23454\"/>\n"); CHKERRQ(ierr);
     ierr = PetscViewerASCIIPrintf(viewer, "    " "<Domain>\n\n"); CHKERRQ(ierr);
-    
-    // write topology
+
+    // write topology; always use 3DRectMesh
     ierr = PetscViewerASCIIPrintf(viewer, "    "
             "<Topology Name=\"%s Topo\" ", name.c_str()); CHKERRQ(ierr);
-    ierr = PetscViewerASCIIPrintf(viewer, 
-            "TopologyType=\"%dDRectMesh\" ", (dim==3)?3:2); CHKERRQ(ierr);
-    ierr = PetscViewerASCIIPrintf(viewer, 
-            "NumberOfElements=\"%s&Ny; &Nx;\"/>\n\n", 
-            (dim==3)?"&Nz; ":""); CHKERRQ(ierr);
-    
-    // write geometry
+    ierr = PetscViewerASCIIPrintf(viewer,
+            "TopologyType=\"3DRectMesh\" "); CHKERRQ(ierr);
+    ierr = PetscViewerASCIIPrintf(viewer,
+            "NumberOfElements=\"&Nz; &Ny; &Nx;\"/>\n\n"); CHKERRQ(ierr);
+
+    // write geometry; always use 3D, i.e., VXVYVZ
     ierr = PetscViewerASCIIPrintf(viewer, "    "
             "<Geometry Name=\"%s Geo\" ", name.c_str()); CHKERRQ(ierr);
-    ierr = PetscViewerASCIIPrintf(viewer, 
-            "GeometryType=\"VXVY%s\">\n", (dim==3)?"VZ":""); CHKERRQ(ierr);
-    
+    ierr = PetscViewerASCIIPrintf(viewer,
+            "GeometryType=\"VXVYVZ\">\n"); CHKERRQ(ierr);
+
+    // output grid coordinates
     for(int i=0; i<dim; ++i)
     {
         std::string dir = type::dir2str[type::Dir(i)];
-        ierr = PetscViewerASCIIPrintf(viewer, "    " "    " 
+        ierr = PetscViewerASCIIPrintf(viewer, "    " "    "
                 "<DataItem Dimensions=\"&N%s;\" ", dir.c_str()); CHKERRQ(ierr);
-        ierr = PetscViewerASCIIPrintf(viewer, 
+        ierr = PetscViewerASCIIPrintf(viewer,
                 "Format=\"HDF\" NumberType=\"Float\" Precision=\"8\">\n"); CHKERRQ(ierr);
-        ierr = PetscViewerASCIIPrintf(viewer, "    " "    " "    " 
+        ierr = PetscViewerASCIIPrintf(viewer, "    " "    " "    "
                 "&CaseDir;/grid.h5:/%s/%s\n", name.c_str(), dir.c_str()); CHKERRQ(ierr);
-        ierr = PetscViewerASCIIPrintf(viewer, "    " "    " 
+        ierr = PetscViewerASCIIPrintf(viewer, "    " "    "
                 "</DataItem>\n"); CHKERRQ(ierr);
     }
-    
+
+    if (dim == 2) // we still pretend everything's 3D, so write a dummy z-axis
+    {
+        ierr = PetscViewerASCIIPrintf(viewer, "    " "    "
+                "<DataItem Dimensions=\"&Nz;\" "); CHKERRQ(ierr);
+        ierr = PetscViewerASCIIPrintf(viewer,
+                "Format=\"XML\" NumberType=\"Float\" Precision=\"8\">\n"); CHKERRQ(ierr);
+        ierr = PetscViewerASCIIPrintf(viewer, "    " "    " "    "
+                "0.0\n"); CHKERRQ(ierr);
+        ierr = PetscViewerASCIIPrintf(viewer, "    " "    "
+                "</DataItem>\n"); CHKERRQ(ierr);
+    }
+
     ierr = PetscViewerASCIIPrintf(viewer, "    " "</Geometry>\n\n"); CHKERRQ(ierr);
-    
-    
+
+
     //write temporal grid collection
     ierr = PetscViewerASCIIPrintf(viewer, "    "
             "<Grid GridType=\"Collection\" CollectionType=\"Temporal\">\n\n"); CHKERRQ(ierr);
-    
+
     // write each step
     for(PetscInt t=bg; t<=ed; t+=step)
     {
-        ierr = PetscViewerASCIIPrintf(viewer, "    " "    " 
+        ierr = PetscViewerASCIIPrintf(viewer, "    " "    "
                 "<Grid GridType=\"Uniform\" Name=\"%s Grid\">\n", name.c_str()); CHKERRQ(ierr);
-        
+
         ierr = PetscViewerASCIIPrintf(viewer, "    " "    " "    "
                 "<Time Value=\"%07d\" />\n", t); CHKERRQ(ierr);
         ierr = PetscViewerASCIIPrintf(viewer, "    " "    " "    "
@@ -220,8 +234,7 @@ PetscErrorCode writeSingleXDMF(
                 "<Attribute Name=\"%s\" AttributeType=\"Scalar\" Center=\"Node\">\n",
                 name.c_str()); CHKERRQ(ierr);
         ierr = PetscViewerASCIIPrintf(viewer, "    " "    " "    " "    "
-                "<DataItem Dimensions=\"%s&Ny; &Nx;\" ",
-                (dim==3)?"&Nz; ":""); CHKERRQ(ierr);
+                "<DataItem Dimensions=\"&Nz; &Ny; &Nx;\" "); CHKERRQ(ierr);
         ierr = PetscViewerASCIIPrintf(viewer,
                 "Format=\"HDF\" NumberType=\"Float\" Precision=\"8\">\n"); CHKERRQ(ierr);
         ierr = PetscViewerASCIIPrintf(viewer, "    " "    " "    " "    " "    "
@@ -232,13 +245,13 @@ PetscErrorCode writeSingleXDMF(
                 "</Attribute>\n"); CHKERRQ(ierr);
         ierr = PetscViewerASCIIPrintf(viewer, "    " "    " "</Grid>\n\n"); CHKERRQ(ierr);
     }
-    
+
     ierr = PetscViewerASCIIPrintf(viewer, "    "
             "</Grid>\n\n"); CHKERRQ(ierr);
     ierr = PetscViewerASCIIPrintf(viewer, "    " "</Domain>\n"); CHKERRQ(ierr);
     ierr = PetscViewerASCIIPrintf(viewer, "</Xdmf>\n"); CHKERRQ(ierr);
-    
+
     ierr = PetscViewerDestroy(&viewer); CHKERRQ(ierr);
-    
+
     PetscFunctionReturn(0);
 } // writeSingleXDMF

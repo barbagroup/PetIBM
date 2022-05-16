28d461b1fde91798fea35ba06878108c5f40f979
Pi-Yueh Chuang <pychuang@gwu.edu>
2022-05-16 19:01:53 -0400

    remove GNU automake files

--- autogen.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# --force : rebuild configure script no matter timestamp related to configure.ac
-# --install : copy some missing files (e.g. COPYING, INSTALL)
-# -I config -I m4 : files are placed in those subdirectories
-mkdir -p config m4
-autoreconf --force --install -I config -I m4

f246c9d3a9d2fcfbc92db9f0416c237da39bf948
Pi-Yueh Chuang <pychuang@gwu.edu>
2022-05-20 16:23:23 -0400

    tgv 2d re100: change domain range and improved post-processing

--- examples/navierstokes/taylorgreenvortex2dRe100/postprocessing.py
+++ examples/navierstokes/taylorgreenvortex2dRe100/postprocessing.py
@@ -17,34 +17,35 @@ import h5py
 from matplotlib import pyplot
 
 
-def get_exact_solution(x, y, t, nu, rho, field):
+def get_exact_solution(x, y, t, nu, rho, field, L=1, V0=1.):
     """Exact solution of 2D Taylor-Green vortex.
     """
     if field == "u":
-        return numpy.cos(x) * numpy.sin(y) * numpy.exp(-2.*nu*t)
+        return V0 * numpy.cos(x/L) * numpy.sin(y/L) * numpy.exp(-2.*nu*t/L**2)
     elif field == "v":
-        return - numpy.sin(x) * numpy.cos(y) * numpy.exp(-2.*nu*t)
+        return - V0 * numpy.sin(x/L) * numpy.cos(y/L) * numpy.exp(-2.*nu*t/L**2)
     elif field == "p":
-        return - rho * numpy.exp(-4.*nu*t) * (numpy.cos(2.*x) + numpy.cos(2.*y)) / 4.
+        return - rho * V0**2 * numpy.exp(-4.*nu*t/L**2) * (numpy.cos(2.*x/L) + numpy.cos(2.*y/L)) / 4.
     elif field == "wz":
-        return - 2. * numpy.cos(x) * numpy.cos(y) * numpy.exp(-2.*nu*t)
+        return - 2. * V0 * numpy.cos(x/L) * numpy.cos(y/L) * numpy.exp(-2.*nu*t/L**2) / L
     elif field == "KE":  # kinetic energy
-        return rho * numpy.pi * numpy.pi * numpy.exp(-4.*nu*t)
+        return numpy.pi**2 * L**2 * V0**2 * rho * numpy.exp(-4.*nu*t/L**2)
     elif field == "KEDR":  # kinetic energy dissipation rate
-        return - 4. * nu * rho * numpy.pi * numpy.pi * numpy.exp(-4.*nu*t)
+        return 4. * numpy.pi**2 * V0**2 * nu * rho * numpy.exp(-4.*nu*t/L**2)
+    elif field == "enstrophy":  # enstrophy
+        return 2. * numpy.pi**2 * V0**2 * nu * rho * numpy.exp(-4.*nu*t/L**2)
     else:
         raise ValueError
 
 
-def calculate_ke(u, v):
-    """Simle linear interpolation + summation to calculate total kinetic energy.
+def calculate_mean_kinetic_energy(u, v):
+    """Simle linear interpolation + summation to calculate volumetrically averaged kinetic energy.
     """
     uintrp = (u[:, 1:] + u[:, :-1]) / 2.
     vintrp = (v[1:, :] + v[:-1, :]) / 2.
 
     ny, nx = uintrp.shape
-    area = 4 * numpy.pi * numpy.pi
-    ke = area * numpy.sum(uintrp**2 + vintrp**2) / ny / nx / 2.
+    ke = numpy.sum(uintrp**2 + vintrp**2) / ny / nx / 2.
 
     return ke
 
@@ -57,31 +58,26 @@ def plot_kinetic_energy(workdir, snapshots, nu, rho):
     simke = []
     for time, vals in snapshots.items():
         simtimes.append(time)
-        simke.append(calculate_ke(vals["pred"]["u"], vals["pred"]["v"]))
+        simke.append(calculate_mean_kinetic_energy(vals["pred"]["u"], vals["pred"]["v"]))
 
     times = numpy.linspace(0., 100., 101)
     ke = get_exact_solution(0, 0, times, nu, rho, "KE")  # x, y not important for calc. KE
-    kedr = get_exact_solution(0, 0, times, nu, rho, "KEDR")  # x, y not important for calc. KEDR
+    ke /= (4 * numpy.pi * numpy.pi)  # volumetric average
+
+    # normalization
+    simke = [val / ke[0] for val in simke]  # must before the next line, otherwise ke[0] is gone
+    ke /= ke[0]
 
     workdir.joinpath("figures").mkdir(exist_ok=True)
 
     pyplot.figure(figsize=(8, 6), dpi=166, constrained_layout=True)
     pyplot.plot(times, ke, "k-", lw=2, label="Analytical solution")
-    pyplot.scatter(simtimes, simke, 80, "tab:red", marker="x", label="Simulation")
+    pyplot.scatter(simtimes, simke, 120, "tab:red", marker="x", label="Simulation")
     pyplot.xlabel("Time (seconds)")
-    pyplot.ylabel(r"Kinetic energy ($kg-m^2 / s^2$)")
+    pyplot.ylabel(r"Kinetic energy (normalized)")
     pyplot.title("Total kinetic energy v.s. time")
     pyplot.legend(loc=0)
     pyplot.savefig(workdir.joinpath("figures", f"kinetic-energy.png"), dpi=166)
-    pyplot.close("all")
-
-    pyplot.figure(figsize=(8, 6), dpi=166, constrained_layout=True)
-    pyplot.plot(times, kedr, "k-", lw=3, label="Analytical solution")
-    pyplot.xlabel("Time (seconds)")
-    pyplot.ylabel(r"Kinetic energy dissipation rate ($kg-m^2 / s^3$)")
-    pyplot.title("Kinetic energy dissipation rate v.s. time")
-    pyplot.legend(loc=0)
-    pyplot.savefig(workdir.joinpath("figures", f"kinetic-energy-dissipation-rate.png"), dpi=166)
     pyplot.close("all")
 
 
@@ -154,17 +150,17 @@ def worker(rank, inputs, outputs, kwargs):
 
     while True:
         try:
-            filename = inputs.get(False)  # w/ False, it returns even not getting anything
+            filename = inputs.get(True, 2)
         except multiprocessing.queues.Empty:
-            print(f"[Rank {rank}] has no more work to do.")
-            inputs.close()
-            outputs.close()
-            return
+            print(f"[Rank {rank}] no more work to do, going home")
+            break
 
         print(f"[Rank {rank}] processing {filename.name}.")
         outcome = process_single_file(filename=filename, **kwargs)
         outputs.put(outcome)
         print(f"[Rank {rank}] done processing {filename.name}.")
+
+        inputs.task_done()
 
 
 if __name__ == "__main__":
@@ -184,8 +180,8 @@ if __name__ == "__main__":
         y = {key: dset[f"{key}/y"][...] for key in fields}
 
     # padding the nodes not used in simulation due to periodic BC
-    x["u"] = numpy.concatenate((numpy.zeros(1, float), x["u"]))
-    y["v"] = numpy.concatenate((numpy.zeros(1, float), y["v"]))
+    x["u"] = numpy.concatenate((numpy.full((1,), -numpy.pi, float), x["u"]))
+    y["v"] = numpy.concatenate((numpy.full((1,), -numpy.pi, float), y["v"]))
 
     # convert gridlines to mesh grids
     xy = {key: numpy.meshgrid(x[key], y[key]) for key in fields}
@@ -200,7 +196,7 @@ if __name__ == "__main__":
     ]
 
     # prepare work loads
-    works = multiprocessing.Queue()
+    works = multiprocessing.JoinableQueue()
     for filename in filenames:
         works.put(filename)
 
@@ -218,6 +214,9 @@ if __name__ == "__main__":
         proc.start()
         procs.append(proc)
 
+    # block until all works in `works` are done (i.e., `works` is empty)
+    works.join()
+
     # get result from outcome queue to EMPTY the queue
     # workers won't terminate unitil involved queues (works & outcomes) are empty
     results = {}
@@ -227,10 +226,6 @@ if __name__ == "__main__":
         results[time] = result
     else:
         assert outcomes.empty()
-
-    # block the main program until all workser are done and terminated
-    for proc in procs:
-        proc.join()
 
     # plot kinetic energy
     plot_kinetic_energy(root, results, cfg["flow"]["nu"], 1.0)
